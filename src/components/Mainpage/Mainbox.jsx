88ed39769c5e073a437045d36fc576559edf4bc4
ravi shankar <shankarji1729@gmail.com>
2021-09-07 01:12:09 +0530

    Button add for live show

--- src/components/Mainpage/Mainbox.jsx
+++ src/components/Mainpage/Mainbox.jsx
@@ -1,6 +1,7 @@
 import { data } from "autoprefixer";
 import React from "react";
 import Image from "next/image";
+import { Button } from "react-bootstrap";
 
 function Mainbox() {
   const data = {
@@ -16,7 +17,15 @@ function Mainbox() {
     <div className="tw-ml-2 tw-p-2">
       <div className="tw-relative tw-font-sans parent_transition">
         <img src={data.photo} alt="Mainbox" className="tw-h-40 tw-w-40" />
-        <p className=" tw-absolute tw-z-10 tw-bottom-0 child_transition ">{data.Description}</p>
+        <div className=" tw-absolute tw-z-10 tw-bottom-0 child_transition ">
+          {data.Description}
+          <br />
+          <div className=" tw-absolute tw-bottom-0 tw-ml-6 ">
+            <Button className=" tw-bg-green-600  tw-rounded-full" >
+              watch Live
+            </Button>
+          </div>
+        </div>
       </div>
     </div>
   );
