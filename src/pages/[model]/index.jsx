a77406fcca31664d2ce682c992e64b4ec90fe335
Rohit kumar <rohitjadav1729@gmail.com>
2021-09-17 11:34:12 +0530

    completed profile section| working on modal

--- src/pages/[model]/index.jsx
+++ src/pages/[model]/index.jsx
@@ -5,7 +5,7 @@ import Headermodel from "../../components/model/Headermodel";
 import Livescreen from "../../components/model/Livescreen";
 import { useSidebarUpdate, useSidebarStatus } from "../../app/Sidebarcontext";
 import Sidebar from "../../components/Mainpage/Sidebar";
-import Profile from "../../components/model/profile";
+import ModelProfile from "../../components/model/ModelProfile.jsx";
 
 function index() {
   const sidebarStatus = useSidebarStatus();
@@ -13,9 +13,24 @@ function index() {
     <div className="">
       <Header />
       <Headermodel />
-      <Sidebar/>
+      <Sidebar />
       <Livescreen />
-      <Profile />
+      <ModelProfile profileData={{
+        name: "Neeraj Rai",
+        age: 42,
+        tags: ['Black', "White", "Artist", "Intelligent", 'Black', "White", "Artist", 'Black', "White", "Artist"],
+        categories: ["America", "India", "Bhutan", "USA"]
+      }}
+        dynamicFields={[
+          { title: "Language", value: ["hindi", "english"] },
+          { title: "body type", value: "curvy" },
+          { title: "ethnicity", value: "American" },
+          { title: "hair", value: "black" },
+          { title: "eye color", value: "black" },
+          { title: "social links", value: "What to do ?" },
+          { title: "features", value: ["nice", "artist", "fast"] },
+        ]}
+      />
       <Footer />
     </div>
   );
