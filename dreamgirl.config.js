67409c91154e7b6cbe99b081978ea9c30848d586
Rohit kumar <rohitjadav1729@gmail.com>
2021-10-12 14:10:33 +0530

    presentaton error

--- dreamgirl.config.js
+++ dreamgirl.config.js
@@ -1,3 +1,3 @@
 // export const imageDomainURL = "http://192.168.0.180:8080"
-export const imageDomainURL = "http://192.168.140.198:8080"
-export const imageDomainHost = "192.168.140.198"
+export const imageDomainURL = "http://192.168.25.198:8080"
+export const imageDomainHost = "192.168.25.198"
