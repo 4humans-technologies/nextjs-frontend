4b8b5077d1254fa3a4f73a82758b1cfbdef59802
Rohit kumar <rohitjadav1729@gmail.com>
2021-11-10 11:43:56 +0530

    next build error repair two

--- src/pages/view-stream/[model].js
+++ src/pages/view-stream/[model].js
@@ -2,17 +2,15 @@ import { useRouter } from "next/router"
 import React, { useState } from "react"
 import Footer from "../../components/Mainpage/Footer"
 import Header from "../../components/Mainpage/Header"
-import SecondHeader from "../../components/Mainpage/SecondHeader"
 import Sidebar from "../../components/Mainpage/Sidebar"
 import ModelProfile from "../../components/model/ModelProfile"
 import Recommendation from "../../components/ViewerScreen/Recommendation"
-import CallEndDetails from "../../components/Call/CallEndDetails"
+import LiveScreen from "../../components/model/LiveScreen"
+// import dynamic from "next/dynamic"
 
-import dynamic from "next/dynamic"
-
-const LiveScreen = dynamic(() => import("../../components/model/LiveScreen"), {
-  ssr: false,
-})
+// const LiveScreen = dynamic(() => import("../../components/model/LiveScreen"), {
+//   ssr: true,
+// })
 
 function ViewModelStream() {
   // 👇👇👇 store the models profile value in a state here
