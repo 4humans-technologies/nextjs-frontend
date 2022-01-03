9d1c08d42731a28d9931d61249ddd0c1aa4b3192
Rohit kumar <rohitjadav1729@gmail.com>
2022-01-03 17:31:12 +0530

    fixed model detail header image arror & and added profile scroll into view

--- src/components/ViewerScreen/ModelDetailHeader.js
+++ src/components/ViewerScreen/ModelDetailHeader.js
@@ -5,27 +5,30 @@ function ModelDetailHeader(props) {
   const { profileImage, username, isStreaming, onCall } = props.data
   return (
     <div className="tw-flex tw-justify-start tw-items-center tw-py-1.5 tw-px-4 tw-overflow-x-auto tw-flex-nowrap tw-bg-first-color tw-text-white-color tw-shadow-md tw-sticky">
-      <a href="#model-profile-area">
-        <span className="tw-w-10 tw-h-10 tw-ring-2 tw-ring-dreamgirl-red tw-mr-4 tw-rounded-full tw-overflow-hidden tw-flex-shrink-0">
-          <img
-            src={profileImage}
-            alt=""
-            className="tw-w-full tw-h-full tw-object-cover"
-          />
-        </span>
+      <a
+        href="#model-profile-area"
+        className="tw-w-10 tw-h-10 tw-ring-2 tw-ring-dreamgirl-red tw-mr-4 tw-rounded-full tw-overflow-hidden tw-flex-shrink-0"
+      >
+        <img
+          src={profileImage}
+          alt=""
+          className="tw-w-full tw-h-full tw-object-cover"
+        />
       </a>
       {isStreaming && (
-        <a href="#model-profile-area">
-          <span className="sm:tw-mr-4 tw-mr-2.5 sm:tw-text-base tw-text-sm tw-py-1 tw-rounded tw-font-semibold tw-text-green-color">
-            Live
-          </span>
+        <a
+          href="#model-profile-area"
+          className="sm:tw-mr-4 tw-mr-2.5 sm:tw-text-base tw-text-sm tw-py-1 tw-rounded tw-font-semibold tw-text-green-color hover:tw-text-green-color"
+        >
+          Live
         </a>
       )}
       {onCall && !isStreaming && (
-        <a href="#model-profile-area">
-          <span className="sm:tw-mr-4 tw-mr-2.5 sm:tw-text-base tw-text-sm tw-py-1 tw-rounded tw-font-semibold tw-text-purple-600">
-            On Call
-          </span>
+        <a
+          href="#model-profile-area"
+          className="sm:tw-mr-4 tw-mr-2.5 sm:tw-text-base tw-text-sm tw-py-1 tw-rounded tw-font-semibold tw-text-purple-600 hover:tw-text-purple-600"
+        >
+          On Call
         </a>
       )}
       {!isStreaming && !onCall && (
@@ -36,10 +39,11 @@ function ModelDetailHeader(props) {
           Offline
         </a>
       )}
-      <a href="#model-profile-area">
-        <span className="sm:tw-mr-4 tw-mr-2.5 sm:tw-text-base tw-text-sm tw-capitalize tw-cursor-pointer">
-          {username}
-        </span>
+      <a
+        href="#model-profile-area"
+        className="sm:tw-mr-4 tw-mr-2.5 sm:tw-text-base tw-text-sm tw-capitalize tw-cursor-pointer hover:tw-text-white-color"
+      >
+        {username}
       </a>
       <a
         href="#model-profile-area"
